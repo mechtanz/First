0ce22cd4ff67885fe4080343b555f86cec786375
vigneshm04 <mechtanz@gmail.com>
2022-02-22 14:32:20 +0530

    Initial Codes

---
+++ src/test/java/org/gitprep/GIT.java
@@ -0,0 +1,13 @@
+package org.gitprep;
+
+public class GIT {
+	private void tc1() {
+	System.out.println("1");
+	}
+private void tc2() {
+	System.out.println("2");
+}
+private void tc3() {
+	System.out.println("3");
+}
+}
